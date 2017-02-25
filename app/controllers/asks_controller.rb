40998ad7e64d9bae468c0f96af669e8d9c21d07a
Leo Prieto <leo@leoprieto.com>
2017-02-25 01:41:35 -0300

    Asks CRUD Ability

---
+++ app/controllers/asks_controller.rb
@@ -0,0 +1,50 @@
+class AsksController < ApplicationController
+	before_action :find_ask, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@asks = Ask.all.order("created_at DESC")
+	end
+
+	def show
+	end
+
+	def new
+		@ask = Ask.new
+	end
+
+	def create
+		@ask = Ask.new(ask_params)
+
+		if @ask.save
+			redirect_to @ask, notice: "Exito Guardadamente nuevo Ask!"
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @ask.update(ask_params)
+			redirect_to @ask, notice: "Ask has been updated!"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@ask.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def ask_params
+		params.require(:ask).permit(:title, :description)
+	end
+
+	def find_ask
+		@ask = Ask.find(params[:id])
+	end
+end
